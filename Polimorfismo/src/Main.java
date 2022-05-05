0120506015224001b26c072b7f2fb1873057837b
CaioCapel <102962595+CaioCapel@users.noreply.github.com>
2022-05-05 11:30:54 -0300

    Add files via upload

---
+++ Polimorfismo/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Animal animal= new Cachorro();
+	    animal.Falar();
+		
+		
+		Gato cat = new Gato();
+		cat.Falar();
+
+	}
+
+}
