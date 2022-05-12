4fa7e4b5a06e24df1aad1c1fb684c9f2f4226edc
CaioCapel <102962595+CaioCapel@users.noreply.github.com>
2022-05-12 11:38:35 -0300

    Add files via upload

---
+++ Interface/src/Aplicacao.java
@@ -0,0 +1,27 @@
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+		
+		Rota rota = new Rota();
+		Ferrari ferrari = new Ferrari();
+		Fusca fusca = new Fusca();
+		rota.ir(ferrari);
+		System.out.println();
+		rota.ir(fusca);
+		System.out.println();
+		Automovel automovel = new Ferrari();
+		rota.ir(automovel);
+		System.out.println();
+		automovel = new Fusca();
+		rota.ir(automovel);
+		System.out.println();
+		Vendedor vendedor = new Vendedor();
+		Apartamento apartamento = new Apartamento();
+		
+		vendedor.mostrarPreco(apartamento);
+		vendedor.mostrarPreco(ferrari); 
+		
+	}
+
+}
