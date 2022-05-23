3cf3d039e5dfd335c5ee2804af4cf805f34962fc
CaioCapel <102962595+CaioCapel@users.noreply.github.com>
2022-05-23 10:06:36 -0300

    Add files via upload

---
+++ ContaBancaria/src/ContaCorrente.java
@@ -0,0 +1,33 @@
+public class ContaCorrente extends ContaBancaria{
+	
+
+	private double saldo = ContaBancaria.getSaldo();
+
+	
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	
+	@Override
+	
+	
+	public void depositar(double valor) {
+		// TODO Auto-generated method stub
+		saldo +=valor;
+	}
+
+	@Override
+	public void sacar(double valor) {
+		// TODO Auto-generated method stub
+		saldo -=valor;
+	}
+
+
+	@Override
+	public double CalcularSaldo() {
+		// TODO Auto-generated method stub
+		return saldo-(saldo*0.10);
+	}
+	
+}
