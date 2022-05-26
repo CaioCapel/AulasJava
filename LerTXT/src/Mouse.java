a7e47bb4cf77699e2202f08089fe2ed481157c04
CaioCapel <102962595+CaioCapel@users.noreply.github.com>
2022-05-26 11:40:12 -0300

    Add files via upload

---
+++ LerTXT/src/Mouse.java
@@ -0,0 +1,64 @@
+
+public class Mouse {
+
+	
+
+	private String funciona;
+	private String qtoBotoes;
+	private String tamanho;
+	
+	
+	
+	public Mouse(String funciona, String qtoBotoes, String tamanho) {
+		super();
+		this.funciona = funciona;
+		this.qtoBotoes = qtoBotoes;
+		this.tamanho = tamanho;
+	}
+	
+	@Override
+	public String toString() {
+		return "Mouse: funciona = " + funciona + ", qtoBotoes = " + qtoBotoes + ", tamanho = " + tamanho + "]\n";
+	}
+	
+	public Mouse(String texto) {
+		
+		String[] arrayAuxiliar1 = texto.split(" , ");
+		String[] arrayAuxiliar2 = arrayAuxiliar1[0].split(" = ");
+		this.funciona = arrayAuxiliar2[1].trim();
+		
+		String[] arrayAuxiliar3 = arrayAuxiliar1[1].split(" = ");
+		this.qtoBotoes = arrayAuxiliar3[1].trim();
+		
+		String[] arrayAuxiliar4 = arrayAuxiliar1[2].split(" = ");
+		this.tamanho = arrayAuxiliar4[1].trim();
+	}
+	
+
+	public String isFunciona() {
+		return funciona;
+	}
+
+	public void setFunciona(String funciona) {
+		this.funciona = funciona;
+	}
+
+	public String isQtoBotoes() {
+		return qtoBotoes;
+	}
+
+	public void setQtoBotoes(String qtoBotoes) {
+		this.qtoBotoes = qtoBotoes;
+	}
+
+	public String getTamanho() {
+		return tamanho;
+	}
+
+	public void setTamanho(String tamanho) {
+		this.tamanho = tamanho;
+	}
+	
+
+	
+}
