d2eec23855a884d57e7efa2dd13f120365b46a12
CaioCapel <102962595+CaioCapel@users.noreply.github.com>
2022-06-08 20:26:09 -0300

    Update Matematica.java

--- ElementosStaticos/src/Matematica.java
+++ ElementosStaticos/src/Matematica.java
@@ -1,11 +1,14 @@
 
 public class Matematica {
-
+	//criado um método estático
+	//Static serve para nao precisar instanciar um objeto para acessar o metodo
 	public static int somar(int a, int b) {
+		//esse método retorna soma
 		return a+b;
 	}
 	
 	public static int subtrair(int a, int b) {
+		//já esse retorna subtração
 		return a-b;
 	}
 }
