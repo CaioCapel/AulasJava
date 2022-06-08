c3dfc5989f0b478c48b0b29a0699aa600fdf59e5
CaioCapel <102962595+CaioCapel@users.noreply.github.com>
2022-06-08 20:47:19 -0300

    Update Carro.java

--- Heranca/src/Carro.java
+++ Heranca/src/Carro.java
@@ -1,14 +1,18 @@
-
+//a classe carro extend da classe veiculo
 public class Carro extends Veiculo {
 	
 		private boolean quatroPortas;
 
+		//usamos o método imprimir pois o carro possui uma variável a mais
 		@Override
 		public void imprimir() {
+			//herda todo o código do método imprimir
 			super.imprimir();
+			//adicionado para saber se o carro tem 4 portas 
 			System.out.println("Tem 4 Portas: " + quatroPortas);
 		}
 		
+		//getter e setter da variável boolean quatroPortas
 		public boolean isQuatroPortas() {
 			return quatroPortas;
 		}
