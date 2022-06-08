56726090fc51543189412056928291be328cce4f
CaioCapel <102962595+CaioCapel@users.noreply.github.com>
2022-06-08 19:55:51 -0300

    Update Moeda.java

--- ClassesAbstratas/src/Moeda.java
+++ ClassesAbstratas/src/Moeda.java
@@ -1,6 +1,7 @@
-
+//a classe moeda se extende de item
 public class Moeda extends Item {
 
+	//foi usado o método pegar da classe item para se adequar as necessidades da classe
 	@Override
 	public void pegar() {
 		// TODO Auto-generated method stub
