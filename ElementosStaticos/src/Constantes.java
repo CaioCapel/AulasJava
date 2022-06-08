4109b70e9a00f178270eb4c84827782829d02054
CaioCapel <102962595+CaioCapel@users.noreply.github.com>
2022-06-08 20:20:58 -0300

    Update Constantes.java

--- ElementosStaticos/src/Constantes.java
+++ ElementosStaticos/src/Constantes.java
@@ -2,8 +2,11 @@
 public class Constantes {
 
 //	public static final double MEDIA_DA_PROVA = 7.0;
+	
+	//final é um método que não pode ser sobrescrito
 	public static final double MEDIA_DA_PROVA;
 	
+	//foi definido que a média é uma constante, significa que nunca vai mudar
 	static {
 		MEDIA_DA_PROVA = 7.0;
 	}
