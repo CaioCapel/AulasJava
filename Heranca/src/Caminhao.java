86a71632606e9041afdd40150057043ea5174e16
CaioCapel <102962595+CaioCapel@users.noreply.github.com>
2022-06-08 20:49:37 -0300

    Update Caminhao.java

--- Heranca/src/Caminhao.java
+++ Heranca/src/Caminhao.java
@@ -4,6 +4,7 @@ public class Caminhao extends Veiculo {
 	//foi sobrescrito o método buzinar pois é diferente do padrão definido
 	@Override
 	public void buzinar() {
+		//mostra a mensagem na tela
 		System.out.println("FOOOMM");
 	}
 }
