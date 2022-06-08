060a644e62f5d2ca9a73d3c18d7607029a0b5e5f
CaioCapel <102962595+CaioCapel@users.noreply.github.com>
2022-06-08 19:55:13 -0300

    Update Cogumelo.java

--- ClassesAbstratas/src/Cogumelo.java
+++ ClassesAbstratas/src/Cogumelo.java
@@ -1,7 +1,7 @@
 //a classe cogumelo é uma classe que se extende da super classe item e possui os mesmos métodos que ela;
 public class Cogumelo extends Item {
 
-	//esta sobrescrevendo o método pegar da classe item
+	//foi usado o método pegar da classe item para se adequar as necessidades da classe
 	@Override
 	public void pegar() {
 		// TODO Auto-generated method stub
