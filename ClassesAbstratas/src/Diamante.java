5b2770c203f4ad75eb0dec15b571cad54fee28ca
CaioCapel <102962595+CaioCapel@users.noreply.github.com>
2022-06-08 19:54:37 -0300

    Update Diamante.java

--- ClassesAbstratas/src/Diamante.java
+++ ClassesAbstratas/src/Diamante.java
@@ -1,6 +1,7 @@
-
+// A classe diamante extende de Item
 public class Diamante extends Item {
 
+	//foi usado o método pegar da classe item para se adequar as necessidades da classe
 	@Override
 	public void pegar() {
 		// TODO Auto-generated method stub
