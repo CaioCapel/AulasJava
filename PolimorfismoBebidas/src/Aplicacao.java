f6eade114ea948d4f0d31590d06115c557b4fbfe
CaioCapel <102962595+CaioCapel@users.noreply.github.com>
2022-06-08 20:55:44 -0300

    Update Aplicacao.java

--- PolimorfismoBebidas/src/Aplicacao.java
+++ PolimorfismoBebidas/src/Aplicacao.java
@@ -3,12 +3,17 @@ public class Aplicacao {
 
 	public static void main(String[] args) {
 		
+		//instanciando a classe preparador
 		Preparador preparador = new Preparador();
 		
+		//instanciando o método leiteQuente
 		LeiteQuente leiteQuente = new LeiteQuente();
+		//chamando o método prepararBebida e passando os parâmetros de LeiteQuente
 		preparador.prepararBebida(leiteQuente);
 		
+		//instanciando o método refrigerante
 		Refrigerante refrigerante = new Refrigerante();
+		//chamando o método prepararBebida e passando os parâmetros de Refrigerante
 		preparador.prepararBebida(refrigerante);
 		
 	}
