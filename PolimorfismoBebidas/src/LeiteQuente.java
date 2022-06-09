8c3a8c3bf82b15b98536bc29dbc7030cafbac3f4
CaioCapel <102962595+CaioCapel@users.noreply.github.com>
2022-06-08 21:01:08 -0300

    Update LeiteQuente.java

--- PolimorfismoBebidas/src/LeiteQuente.java
+++ PolimorfismoBebidas/src/LeiteQuente.java
@@ -1,11 +1,13 @@
-
+//LeiteQuente se extende de bebida
 public class LeiteQuente extends Bebida{
 
 	public LeiteQuente() {
+		//super indica que esta sendo puxado método de outra classe
 		super("Leite Quente", true);
 		
 	}
 
+	//usando o método preparar e adicionando um código específico dessa classe
 	@Override
 	public void preparar() {
 		super.preparar();
