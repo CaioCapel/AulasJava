07b33169089c999a0580b7c08dd29c85dc2467cb
CaioCapel <102962595+CaioCapel@users.noreply.github.com>
2022-06-09 15:12:55 -0300

    Update Cachorro.java

--- Polimorfismo/src/Cachorro.java
+++ Polimorfismo/src/Cachorro.java
@@ -1,13 +1,14 @@
 
 public class Cachorro extends Animal {
 
+	//override do metodo falar
 	@Override
 	public void Falar() {
-		System.out.println("AUAU");
+		System.out.println("AUAU");//mostra na tela
 	}
-	
+	//metodo morder da classe cachorro
 	public void Morder() {
-		System.out.println("NHEW");
+		System.out.println("NHEW");//mostra na tela
 	}
 	
 
