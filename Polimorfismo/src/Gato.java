0fb14bb4e1d75937c7072998eb0ce3adcfad2fff
CaioCapel <102962595+CaioCapel@users.noreply.github.com>
2022-06-09 15:14:15 -0300

    Update Gato.java

--- Polimorfismo/src/Gato.java
+++ Polimorfismo/src/Gato.java
@@ -1,9 +1,9 @@
-
+//gato extende de animal
 public class Gato extends Animal{
-
+	//metodo falar da super classe
 	@Override
 	public void Falar() {
-		System.out.println("MIAU");
+		System.out.println("MIAU");//mostra na tela
 	}
 	
 	
