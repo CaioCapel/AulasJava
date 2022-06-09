c55add8e89c72fa058d25baff3929255e6c2da4e
CaioCapel <102962595+CaioCapel@users.noreply.github.com>
2022-06-09 15:16:07 -0300

    Update Main.java

--- Polimorfismo/src/Main.java
+++ Polimorfismo/src/Main.java
@@ -10,16 +10,16 @@ public class Main {
 //		Gato cat = new Gato();
 //		cat.Falar();
 
-		
+		//instanciando objetos
 		Gato gato=new Gato();
 		Cachorro cachorro = new Cachorro();
-		falar(gato);
-		falar(cachorro);
+		falar(gato);//usa o falar de gato
+		falar(cachorro);//usa o falar de cachorro
 	}
 	public static void falar(Animal a) {
-		a.Falar();
+		a.Falar();//usa falar de animal 
 		
-		if(a instanceof Cachorro) {
+		if(a instanceof Cachorro) {//com a variavel a alem de falar ele irá morder tambem
 			Cachorro c = (Cachorro) a;
 			c.Morder();
 		}	
