5c7216eb3ec76f7f3b54194c8bd686bec4cbbdda
CaioCapel <102962595+CaioCapel@users.noreply.github.com>
2022-06-09 13:54:53 -0300

    Update Main.java

--- Cadastro/src/Main.java
+++ Cadastro/src/Main.java
@@ -12,12 +12,13 @@ public class Main {
 
 	public static void main(String[] args) throws FileNotFoundException, IOException {
 
-		try (Scanner teclado = new Scanner(System.in)) {
-			List<Pessoa> listaPessoas = new ArrayList<Pessoa>();
+		try (Scanner teclado = new Scanner(System.in)) { //instanciando um scanner
+			List<Pessoa> listaPessoas = new ArrayList<Pessoa>();//instanciando uma lista
 
-			for (int x = 0; x < 4; x++) {
-				Pessoa pessoa = new Pessoa();
-				pessoa.setEndereco(new Endereco());
+			for (int x = 0; x < 4; x++) {//cada vez que fizer o processo adiciona mais uma variÃ¡vel
+				
+				Pessoa pessoa = new Pessoa();//instanciando pessoa 
+				pessoa.setEndereco(new Endereco());//instanciando um endereco dentro do setter
 
 				System.out.println("Digite seu nome: ");
 				pessoa.setNome(teclado.next());
@@ -27,39 +28,39 @@ public class Main {
 
 				System.out.println("Digite 1 para o sexo Masculino e Digite 2 para o sexo feminino");
 				int opcao = 0;
-				while (opcao != 1 && opcao != 2) {
+				while (opcao != 1 && opcao != 2) {//enquanto for diferente de 1 ou 2 ele executa as acoes
 					opcao = teclado.nextInt();
 					if (opcao == 1)
-						pessoa.setSexo(Sexo.MASCULINO);
+						pessoa.setSexo(Sexo.MASCULINO);//escolhe o sexo masculino
 
 					else if (opcao == 2)
-						pessoa.setSexo(Sexo.FEMININO);
-					else {
-						System.out.println("Sexo inválido");
+						pessoa.setSexo(Sexo.FEMININO);//escolhe o sexo feminino 
+					else {//caso nao digite nem 1 ou 2 faca 
+						System.out.println("Sexo invÃ¡lido");
 						System.out.println("Digite 1 para o sexo Masculino e Digite 2 para o sexo feminino");
 					}
 				}
 
-				System.out.println("Digite a rua do seu endereço");
-				pessoa.getEndereco().setRua(teclado.next());
+				System.out.println("Digite a rua do seu endereÃ§o");
+				pessoa.getEndereco().setRua(teclado.next()); //pega o endereco e seta a rua com a instancia teclado
 
-				System.out.println("Digite o numero do seu endereço");
-				pessoa.getEndereco().setNumero(teclado.nextInt());
+				System.out.println("Digite o numero do seu endereÃ§o");
+				pessoa.getEndereco().setNumero(teclado.nextInt());//pega o endereco e seta o numero com a instancia teclado
 
-				System.out.println("Digite o bairro do seu endereço");
-				pessoa.getEndereco().setBairro(teclado.next());
+				System.out.println("Digite o bairro do seu endereÃ§o");
+				pessoa.getEndereco().setBairro(teclado.next());//pega o endereco e seta o bairro com a instancia teclado
 
-				listaPessoas.add(pessoa);
+				listaPessoas.add(pessoa);//adiciona pessoa a lista
 			}
 
-			try (BufferedWriter escrever = new BufferedWriter(new FileWriter("Pessoas.txt"))) {
+			try (BufferedWriter escrever = new BufferedWriter(new FileWriter("Pessoas.txt"))) {//transcreve o arquivo para txt
 				for (Pessoa pessoa2 : listaPessoas)
-					escrever.write(pessoa2.toString());
+					escrever.write(pessoa2.toString());// to string para mostrar a lista de pessoas que contem as informacoes delas
 			}
 
-			List<Pessoa> ListaPessoas2 = new ArrayList<Pessoa>();
+			List<Pessoa> ListaPessoas2 = new ArrayList<Pessoa>();//cria uma nova lista
 
-			try (BufferedReader reader = new BufferedReader(new FileReader("Pessoas.txt"))) {
+			try (BufferedReader reader = new BufferedReader(new FileReader("Pessoas.txt"))) {//le o arquivo txt
 				String line;
 				String string = "";
 
