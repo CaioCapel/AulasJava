250dc79378cc54f269ad42906b6a07a32388b24b
CaioCapel <102962595+CaioCapel@users.noreply.github.com>
2022-06-08 21:18:07 -0300

    Update ContaBancaria.java

--- ModificadorAcesso/src/ContaBancaria.java
+++ ModificadorAcesso/src/ContaBancaria.java
@@ -1,20 +1,23 @@
 
 public class ContaBancaria {
 
+	//atributos
 	private int numConta;
 	private boolean ativo;
 	private double saldo;
 
-	public ContaBancaria() {
-		ativo = true;
-		saldo = 1.0;
-	}
+//	public ContaBancaria() {
+//		ativo = true;
+//		saldo = 1.0;
+//	}
 
+	//construtor que recebe o saldo e ve se a conta está ativa 
 	public ContaBancaria(boolean ativo, double saldo) {
 		this.ativo = ativo;
 		this.saldo = saldo;
 	}
-
+	
+	//mostra o valor recebido e soma com o saldo que já tinha
 	public void receber(double valor) {
 		saldo += valor;
 
@@ -23,7 +26,8 @@ public class ContaBancaria {
 	public double saldo() {
 		return saldo;
 	}
-
+	
+	//mostra o valor que esta saindo da conta
 	public void dar(double valor) {
 		if (saldo >= valor)
 			saldo -= valor;
