9b6f6077d78fd69fa810d019a531ff1df3b3e991
CaioCapel <102962595+CaioCapel@users.noreply.github.com>
2022-06-09 14:30:30 -0300

    Update Aplicacao.java

--- ConstrutorDeHorario/src/Aplicacao.java
+++ ConstrutorDeHorario/src/Aplicacao.java
@@ -3,16 +3,16 @@ public class Aplicacao {
 
 	public static void main(String[] args) {
 		
-		Data d1 = new Data(10, 03, 200, 0, 30, 10);
-		d1.imprimir(Data.FORMATO_12H);
+		Data d1 = new Data(10, 03, 200, 0, 30, 10);//instanciando objeto data1
+		d1.imprimir(Data.FORMATO_12H);//imprimindo a hora para mostrar na tela
 		d1.imprimir(Data.FORMATO_24H);
 		
-		Data d2 = new Data(15, 06, 2000, 23, 15, 20);
-		d2.imprimir(Data.FORMATO_12H);
+		Data d2 = new Data(15, 06, 2000, 23, 15, 20);//instanciando objeto data2
+		d2.imprimir(Data.FORMATO_12H);//imprimindo a hora para mostrar na tela
 		d2.imprimir(Data.FORMATO_24H);
 		
-		Data d3 = new Data(5, 10, 2005);
-		d3.imprimir(Data.FORMATO_12H);
+		Data d3 = new Data(5, 10, 2005);//instanciando objeto data3
+		d3.imprimir(Data.FORMATO_12H);//imprimindo a hora para mostrar na tela
 		d3.imprimir(Data.FORMATO_24H);
 		
 		
