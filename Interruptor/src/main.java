7ff90b00869a20b12dbb044f9f1219ff95ee8f21
CaioCapel <102962595+CaioCapel@users.noreply.github.com>
2022-06-09 14:51:28 -0300

    Update main.java

--- Interruptor/src/main.java
+++ Interruptor/src/main.java
@@ -4,9 +4,9 @@ public class main {
 	public static void main(String[] args) {
 		
 		
-		Lampada Lampada = new Lampada();
+		Lampada Lampada = new Lampada();//instanciando objeto lampada
 
-		    
+		    //mostrando o estado da lampada 
 		    Lampada.Ligar();
 		    System.out.println("----------------------");
 		    Lampada.Desligar();
