7be64fe18e43bc6f80046955c54d78e6c335f1d8
CaioCapel <102962595+CaioCapel@users.noreply.github.com>
2022-06-09 14:09:26 -0300

    Update AreaCalculavel.java

--- FigurasGeometricas/src/AreaCalculavel.java
+++ FigurasGeometricas/src/AreaCalculavel.java
@@ -1,6 +1,6 @@
-
+//uma interface que vai receber um metodo
 public interface AreaCalculavel {
-	
+	//esse metodo vai ser melhor definido melhor nas classes que utilizarem ele 
 	public void calcularArea();
 
 }
