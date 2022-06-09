f64ac81de1cb98ce0f8513bdcc0b198a1ae4d008
CaioCapel <102962595+CaioCapel@users.noreply.github.com>
2022-06-09 14:47:50 -0300

    Update Vendedor.java

--- Interface/src/Vendedor.java
+++ Interface/src/Vendedor.java
@@ -1,8 +1,9 @@
 
 public class Vendedor {
 
+	//metodo que mostra preco
 	public void mostrarPreco(ItemCaro caro) {
-		System.out.println("O valor é " + caro.getPreco());
+		System.out.println("O valor Ã© " + caro.getPreco());
 	}
 	
 }
