acde8fa5899c230991b80b552fe24f156fd68f15
CaioCapel <102962595+CaioCapel@users.noreply.github.com>
2022-06-09 13:32:27 -0300

    Update Main.java

--- Enum/src/Main.java
+++ Enum/src/Main.java
@@ -1,6 +1,6 @@
 
 public class Main {
-
+	//criacao da constante 
 	public static final double PI = 3.14;
 	
 	private enum Sexo{
@@ -8,21 +8,26 @@ public class Main {
 	}
 
 	public static void main(String[] args) {
-
+	//cada constante que criar fora do enum tem que guardar em algum lugar 
 		double pi = Main.PI;
 
 		DiaDaSemana hoje = DiaDaSemana.SEGUNDA;
 
+		//pega o numero da semana e o dia
 		int num = DiaDaSemana.SEGUNDA.getNum();
 
+		//variavel num que mostra o numero do dia da semana 
 		System.out.println(num);
 
+		//guardando o nome "TERCA" na variavel s 
 		String s = "TERCA";
 		
 		hoje =DiaDaSemana.valueOf(s);
 		
+		//mostra o dia de hoje, no caso terca
 		System.out.println(hoje.getNum());
 		
+		//sexo do tipo feminino e masculino 
 		Sexo sexo = Sexo.FEMININO;
 		sexo = Sexo.MASCULINO; 
 
